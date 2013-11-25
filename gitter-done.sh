7bf199347cb08c3feb7fd68c0affa02f3bba36a9
= <wufufufu@gmail.com>
2013-11-24 21:56:38 -0500

    Resolved caching issue

---
+++ gitter-done.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Config
+# ##############################
+
+OCCURRENCES=5
+OUT_OF=7
+COMMIT_MESSAGES=("Minor changes" "Changed configuration"  "Bug fixes"  "Resolved caching issue"  "Bug fixed."  "Small changes"  "Added documentation."  "Organized code"  "Rewrote section")
+
+# Logic
+# ##############################
+
+(( decision = RANDOM % OUT_OF + 1 ))
+if (( decision <= OCCURRENCES )); then
+
+	messages_length=${#COMMIT_MESSAGES[@]} 
+
+	script_dir="$( cd "$( /usr/bin/dirname "$0" )" && pwd )"
+	script_name=`basename $0`
+
+	random_index=$RANDOM
+	let "random_index %= $messages_length"
+
+	cp $script_dir/$script_name $script_dir/$script_name.tmp
+	sed '$ d' $script_dir/$script_name.tmp > $script_dir/$script_name
+	rm -f $script_dir/$script_name.tmp
+
+	echo "#$RANDOM $RANDOM $RANDOM $RANDOM" >> $script_dir/$script_name
+
+	git add -A
+	git commit -m "${COMMIT_MESSAGES[$random_index]}"
+	git push origin master
+fi
+
+#25070 31821 21655 15527
